82ca79b1a166d054e2465b08236f3758e6add40b
zahriaan <abelmuhammadzahrian_2327240042@mhs.mdp.ac.id>
2026-04-02 19:56:49 +0700

    Pertemuan 12

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -1,13 +1,61 @@
+import 'package:cepu_app/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<void> signOut(BuildContext context)async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushAndRemoveUntil(context,
+    MaterialPageRoute(builder: (context) => SignInScreen()),
+    (route) => false 
+    );
+  }
+
+  String? _idToken = "";
+  String? _uid = "";
+  String? _email = "";
+  Future<void> getFirebaseAuthUser() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if(user != null) {
+      _uid = user.uid;
+      _email = user.email;
+      await user.getIdToken(true).then((v) => {setState(() {
+        _idToken = v;
+      }),
+      },
+      );
+    }
+  } 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("HomeScreen")),
-      body: const Center(child: Text("You Have Been Signed In!")),
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: (){
+              signOut(context);
+            },icon: const Icon(Icons.logout),
+          ),
+        ],
+        ),
+      body: Center(
+        child : Column(
+          children: [
+            Text("You Have Been Signed In with Token Id : ${_idToken!}"),
+            Text("Current User : ${_uid!}"),
+            Text("Current Email : ${_email!}"),
+          ],
+        ),
+      ),
     );
   }
 }
