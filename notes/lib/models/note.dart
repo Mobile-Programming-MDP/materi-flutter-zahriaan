0120ccd00865a94f53e02d052c53259b179feac0
zahriaan <abelmuhammadzahrian_2327240042@mhs.mdp.ac.id>
2026-04-22 13:08:46 +0700

    Pertemuan 17

---
+++ notes/lib/models/note.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Note {
+  String? id;
+  final String title;
+  final String description;
+  String? imageBase64;
+  Timestamp? createdAt;
+  Timestamp? updatedAt;
+
+  Note({
+    this.id,
+    required this.title,
+    required this.description,
+    this.imageBase64,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory Note.fromDocument(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return Note(
+      id: doc.id,
+      title: data['title'],
+      description: data['description'],
+      imageBase64: data['image_base_64'],
+      createdAt: data['created_at'] as Timestamp,
+      updatedAt: data['updated_at'] as Timestamp,
+    );
+  }
+
+  Map<String, dynamic> toDocument() {
+    return {
+      'title': title,
+      'description': description,
+      'image_base64': imageBase64,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+    };
+  }
+}
